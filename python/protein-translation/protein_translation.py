c704d0c260b39f7ac6db93056eab0a75f2aa4efc
TROLLhard556 <trollhard556@gmail.com>
2021-12-01 21:04:21 -0500

    finished protein-translation, started gigasecond

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,33 @@
+import re
 def proteins(strand):
-    pass
+    translation = {
+        "AUG": "Methionine",
+        "UUU": "Phenylalanine",
+        "UUC": "Phenylalanine",
+        "UUA": "Leucine",
+        "UUG": "Leucine",
+        "UCU": "Serine",
+        "UCC": "Serine",
+        "UCA": "Serine",
+        "UCG": "Serine",
+        "UAU": "Tyrosine",
+        "UAC": "Tyrosine",
+        "UGU": "Cysteine",
+        "UGC": "Cysteine",
+        "UGG": "Tryptophan",
+        "UAA": "STOP",
+        "UAG": "STOP",
+        "UGA": "STOP"
+    }
+    codons = re.findall('...', strand)
+
+    results = []
+
+    for i in codons:
+        if i in translation.keys():
+            if translation[i] == "STOP":
+                break
+            else:
+                results.append(translation[i])
+
+    return results
